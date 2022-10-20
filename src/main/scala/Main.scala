1c77381fc61958ef42211cd81fad0cb7d87b4b66
Hoang-Duc DUONG <hoang-duc.duong@edu.esiee.fr>
2022-10-20 08:09:17 +0200

    update duongh

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,27 @@
+
+trait Parser[A]:
+  /** parse with this and then parse with pb. */
+  def ~[B](pb: => Parser[B]): Parser[(A, B)] = this.
+  /** parse with this, or parse with pb if this fails. */
+  def |[B](pb: Parser[B]): Parser[Either[A, B]] = ???
+  /** try to parse with this. It does not fail if the parsing did not work. */
+  def ? : Parser[Option[A]] = ???
+  /** use this to parse multiple times, until it does not apply. */
+  def repeat: Parser[List[A]] = ???
+  /** convert the value output by the parser. */
+  def map[B](f: A => B): Parser[B] = ???
+  def flatMap[B](f: A => Parser[B]): Parser[B] = ???
+
+  final def parse(s: String): ParseResult[A] = parse(Input(s))
+  def parse(input: Input): ParseResult[A]
+
+
+val numPattern = "[0-9]+".r
+val address = "123 Main Street Suite 101"
+val match1 = numPattern.findFirstIn(address)
+
+
+
 // Input of a parser
 // * data: represents the input string
 // * offset: is the pointer on the string, that the parser increases during its processing
